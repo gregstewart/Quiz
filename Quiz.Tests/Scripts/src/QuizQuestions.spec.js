87c7d7970ff20eb97e3cae09a1b3356e0480d534
Greg Stewart <gregs@tcias.co.uk>
2013-11-11 22:59:14 +0000

    Added a code to handle post to create a new quiz and set the collection uri the response in the location header

--- Quiz.Tests/Scripts/src/QuizQuestions.spec.js
+++ Quiz.Tests/Scripts/src/QuizQuestions.spec.js
@@ -1,35 +1,61 @@
 describe('QuizQuestions', function () {
-   beforeEach(function () {
-     this.collection = new Quiz.QuizQuestions();
-     this.response = [
-       {
-         questionId: 0,
-         question: 'some question',
-         answers: [
+  beforeEach(function () {
+    this.collection = new Quiz.QuizQuestions();
+    this.response = [
+      {
+        questionId: 0,
+        question: 'some question',
+        answers: [
           'answer 1',
           'answer 2',
           'answer 3'
-         ]
-       }
-     ];
-   });
+        ]
+      }
+    ];
+  });
 
   afterEach(function () {
   });
 
-  it('initialises with an empty collection', function() {
+  it('initialises with an empty collection', function () {
     expect(this.collection.length).toBe(0);
   });
 
+  describe('askForNewQuiz', function () {
+    beforeEach(function () {
+      this.server = sinon.fakeServer.create();
+    });
+
+    afterEach(function () {
+      this.server.restore();
+    });
+
+    it('sets the collection URI with the response header value from the service', function (done) {
+
+      var url = "/quiz/";
+      this.server.respondWith("POST", url,
+          [201, { "Content-Type": "application/json",
+            "Location": url + "some-id" },
+            'null']);
+      this.collection.askForNewQuiz();
+      this.server.respond();
+
+      expect(this.collection.url).toBe(url + "some-id");
+
+    });
+  });
+
   describe('fetch questions from server', function () {
-     beforeEach(function () {
-       this.server = sinon.fakeServer.create();
-       this.server.respondWith("GET", "/quiz/some-id",
-           [200, { "Content-Type": "application/json" },
-             JSON.stringify(this.response)]);
-       this.collection.fetch();
-       this.server.respond();
-     });
+    beforeEach(function () {
+      this.server = sinon.fakeServer.create();
+      var url = "/quiz/some-id";
+      this.server.respondWith("GET", url,
+          [200, { "Content-Type": "application/json" },
+            JSON.stringify(this.response)]);
+      this.collection.url = url;
+      this.collection.fetch();
+      this.server.respond();
+    });
 
     afterEach(function () {
       this.server.restore();
@@ -37,7 +63,6 @@ describe('QuizQuestions', function () {
 
     it('populates the collection from the response', function () {
       expect(this.collection.length).toBe(1);
-
     });
 
     it('sets the correct questionId', function () {
--- Quiz/Scripts/src/QuizQuestions.js
+++ Quiz/Scripts/src/QuizQuestions.js
@@ -1,6 +1,14 @@
 var Quiz = Quiz || {};
 
 Quiz.QuizQuestions = Backbone.Collection.extend({
-//  model: Questions
-  url: '/quiz/some-id'
+  url: '/quiz/',
+
+  askForNewQuiz: function () {
+    var self = this;
+    resp = this.create();
+    resp.on('sync', function () {
+      self.url = arguments[2].xhr.getResponseHeader('Location');
+    });
+  }
+
 });
